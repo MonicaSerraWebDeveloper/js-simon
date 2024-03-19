ad0a2d5b064b4c9367eddf9dbc96800404dcf8bf
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-19 14:13:05 +0100

    start init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// Creiamo un array vuoto 
+// Prendiamo la funzione che genera dei numeri random
+// Inseriamo i numeri random nell'Array 
+// Assicuriamoci che i numeri random siano diversi 
+// Mostriamoli in pagina nel DOM in sync (al caricamento)
+// Stabiliamo con un setTimeOut che dopo 30 secondi l'array scompare
+// Dopo 30 secondi scompaiono i numeri e compare un prompt in cui 
+// L'utente scrive i numeri che ricorda 
+// Verifichiamo quanti e quali numeri sono giusti 
+// Stabiliamo che SE i numeri scritti nel prompt sono presenti nell'Array
+// Allora diciamo all'utente con un alert i numeri indovinati e quanti da 0 a 5
+// Sono giusti
